c8ddaf7d16c618050e90baaa0935da99d02936de
denisha-we3vision <denisha.we3vision@gmail.com>
2025-04-10 17:49:10 +0530

    Added new project

--- src/components/sections/portfolio.jsx
+++ src/components/sections/portfolio.jsx
@@ -72,7 +72,7 @@ const Card = ({ category, title, src, animationClass, id }) => {
                 <div className="project-item style-two">
                     <div className="project-image">
                         <img src={src} alt="Project" />
-                        <Link to="/single-project" className="details-btn"><RiArrowRightUpLine /> </Link>
+                        <Link className="details-btn"><RiArrowRightUpLine /> </Link>
                     </div>
                     <div className="project-content">
                         <span className="sub-title">{category}</span>
--- src/utlits/fackData/projectData.jsx
+++ src/utlits/fackData/projectData.jsx
@@ -1,39 +1,63 @@
-import work_1 from "../../assets/images/projects/work1.jpg"
+import work_1 from "../../assets/images/projects/work1.png"
 import work_2 from "../../assets/images/projects/work2.jpg"
 import work_3 from "../../assets/images/projects/work3.jpg"
 import work_4 from "../../assets/images/projects/work4.jpg"
+import work_5 from "../../assets/images/projects/work5.jpg"
+import work_6 from "../../assets/images/projects/work6.jpg"
+import work_7 from "../../assets/images/projects/work7.png"
+
+
 
 export const projectsData = [
     {
         id: 1,
         src: work_1,
         category: "website Design",
-        title: "Mobile Application Design",
+        title: "website Design",
     },
     {
         id: 2,
         src: work_2,
-        category: "website Development",
-        title: "Website Makeup Design",
+        category: "website Design",
+        title: "website Design",
     },
     {
         id: 3,
         src: work_3,
-        category: "Admin Panel",
-        title: "Brand Identity and Motion Design",
+        category: "website Design",
+        title: "website Design",
     },
     {
         id: 4,
         src: work_4,
-        category: "Three js",
-        title: "Mobile Application Design",
+        category: "website Design",
+        title: "website Design",
+    },
+    {
+        id: 5,
+        src: work_5,
+        category: "website Design",
+        title: "website Design",
+    },
+    {
+        id: 6,
+        src: work_6,
+        category: "website development",
+        title: "We3vision Infotech",
+    },
+    {
+        id: 7,
+        src: work_7,
+        category: "admin panel",
+        title: "Admin Panel",
     },
     // {
-    //     id: 5,
-    //     src: work_3,
-    //     category: "Design",
+    //     id: 8,
+    //     src: work_4,
+    //     category: "three js",
     //     title: "Brand Identity and Motion Design",
-    // },
+    // }
+
     // {
     //     id: 6,
     //     src: work_1,
